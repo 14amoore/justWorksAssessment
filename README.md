b5450f6928302019e5dc6641ceef7f6a3c7c95fb
Adam Moore <adamoore@gmail.com>
2023-09-27 12:58:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Justworks Take Home Assessment
+This repo was created as part of completing the Justworks take home assessment. The prompt for the assesment can be found in prompt.md.
---
+++ index.js
@@ -0,0 +1,38 @@
+const cryptoDistribution = async (amount) => {
+  try {
+    const res = await fetch(
+      'https://api.coinbase.com/v2/exchange-rates?currency=USD'
+    );
+    const data = await res.json();
+    const {
+      data: { rates },
+    } = data;
+
+    const { BTC: btcRate } = rates;
+    const { ETH: ethRate } = rates;
+
+    const btcAmount = (amount * 0.7 * btcRate).toFixed(4);
+    const ethAmount = (amount * 0.3 * ethRate).toFixed(4);
+
+    const totalAmount = {
+      Bitcoin: btcAmount,
+      Ethereum: ethAmount,
+    };
+
+    return totalAmount;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  const amountToInvest = 100000;
+  const split = await cryptoDistribution(amountToInvest);
+  if (split) {
+    console.log(
+      `You should buy ${split.Bitcoin} of BTC, and ${split.Ethereum} of ETH to maintain a 70/30 split of the $${amountToInvest} USD you have to invest.`
+    );
+  } else {
+    console.log('Failed to fetch crypto rates.');
+  }
+})();
